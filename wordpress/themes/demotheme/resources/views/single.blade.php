85245c42317922b3cc934bdfacf4ac1b3ac41f13
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-10-02 10:06:37 +0700

    basic controller

--- wordpress/themes/demotheme/resources/views/single.blade.php
+++ wordpress/themes/demotheme/resources/views/single.blade.php
@@ -5,3 +5,14 @@
     @include('partials.content-single-'.get_post_type())
   @endwhile
 @endsection
+
+<!--  -->
+@if($images)
+  <ul>
+      @foreach($images as $images)
+          <li><img src="{{$image['sizes']['thumbnail']}}" alt="{{$image['alt']}}"></li>
+      @endforeach
+  </ul>
+@endif
+
+
