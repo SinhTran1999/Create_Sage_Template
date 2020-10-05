47371a0d7917676e98dba6d7894fa1814e7bbc75
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-10-05 10:27:47 +0700

    init project

--- wordpress/themes/sage/resources/views/single.blade.php
+++ wordpress/themes/sage/resources/views/single.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
+
 @section('content')
   @while(have_posts()) @php the_post() @endphp
-    {{Archive:: title()}}
+    @include('partials.content-single-'.get_post_type())
   @endwhile
 @endsection
-  
