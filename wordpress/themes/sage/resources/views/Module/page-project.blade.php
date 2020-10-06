828ed67ce2c7c9a1568e83e9ca7d1db99eb6d21b
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-10-07 00:06:02 +0700

    get field image from custom field

--- wordpress/themes/sage/resources/views/Module/page-project.blade.php
+++ wordpress/themes/sage/resources/views/Module/page-project.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
   @while (have_posts()) @php the_post() @endphp
-      <section class="py-5 font-sans min-h-screen -mb-10">
+      <section class="py-5 font-sans min-h-screen -mb-10 leading-normal">
         <h2 class="text-2xl mb-4">{{the_title('<h2>','</h2>')}}</h2>
         {!! the_content()!!}
-      </section>
+     
       @php
         $args = [
           'post_per_page' => -1,
@@ -18,15 +18,36 @@
         $the_query = new \WP_Query($args)
       @endphp
 
-      @while ($the_query->have_posts()) 
-        @php $the_query->the_post() @endphp
-        hello
-      @endwhile
+      <div class="flex">
+        @while ($the_query->have_posts()) @php $the_query->the_post() @endphp
+          @php
+          $image = get_field('project_image'); 
+          //  vars
+          $url = $image['url'];
+          $title = $image['title'];
+          $alt = $image['alt'];
+          $caption = $image['caption'];
+
+          // thumbnail, medium, large 
+          $size = ['medium'];
+          $thumb = $image['sizes'][$size];
+          $width = $image['sizes'][$size. '-width'];
+          $height = $image['sizes'][$size.'-height'];
+
+          @endphp
+
+          <div class="w-1/3">
+          <img src= "{{$thumb}}" alt="{{$alt}}" title="{{$title}}">
+          </div>
+
+        @endwhile
+      </div>
+
 
       @php 
         wp_reset_postdata();
-
       @endphp
-  @endwhile
+    @endwhile
+  </section>
 @endsection
   
--- wordpress/uploads/cache/6edacb1b65009333b89390f341a2b2e80b0bb7ba.php
+++ wordpress/uploads/cache/6edacb1b65009333b89390f341a2b2e80b0bb7ba.php
@@ -2,11 +2,11 @@
 
 <?php $__env->startSection('content'); ?>
   <?php while(have_posts()): ?> <?php the_post() ?>
-      <section class="py-5 font-sans min-h-screen -mb-10">
+      <section class="py-5 font-sans min-h-screen -mb-10 leading-normal">
         <h2 class="text-2xl mb-4"><?php echo e(the_title('<h2>','</h2>')); ?></h2>
         <?php echo the_content(); ?>
 
-      </section>
+     
       <?php
         $args = [
           'post_per_page' => -1,
@@ -19,16 +19,37 @@
         $the_query = new \WP_Query($args)
       ?>
 
-      <?php while($the_query->have_posts()): ?> 
-        <?php $the_query->the_post() ?>
-        hello
-      <?php endwhile; ?>
+      <div class="flex">
+        <?php while($the_query->have_posts()): ?> <?php $the_query->the_post() ?>
+          <?php
+          $image = get_field('project_image'); 
+          //  vars
+          $url = $image['url'];
+          $title = $image['title'];
+          $alt = $image['alt'];
+          $caption = $image['caption'];
+
+          // thumbnail, medium, large 
+          $size = ['medium'];
+          $thumb = $image['sizes'][$size];
+          $width = $image['sizes'][$size. '-width'];
+          $height = $image['sizes'][$size.'-height'];
+
+          ?>
+
+          <div class="w-1/3">
+          <img src= "<?php echo e($thumb); ?>" alt="<?php echo e($alt); ?>" title="<?php echo e($title); ?>">
+          </div>
+
+        <?php endwhile; ?>
+      </div>
+
 
       <?php 
         wp_reset_postdata();
-
       ?>
-  <?php endwhile; ?>
+    <?php endwhile; ?>
+  </section>
 <?php $__env->stopSection(); ?>
   
 <?php echo $__env->make('../layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
